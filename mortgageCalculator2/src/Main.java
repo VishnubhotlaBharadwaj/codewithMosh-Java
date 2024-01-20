31ee69d6aff8e5481b0ee4e1516deb4062ae857d
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2024-01-19 20:20:01 -0500

    commit

--- mortgageCalculator2/src/Main.java
+++ mortgageCalculator2/src/Main.java
@@ -1,15 +1,39 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int principal = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.print("Principal ($1k - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a value between 1000 and 1000000");
         }
+
+        System.out.println("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest/ PERCENT/ MONTHS_IN_YEAR;
+
+        System.out.print("Period (Years): ");
+        int years = scanner.nextInt();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal * (((monthlyInterest) * Math.pow((1+ monthlyInterest), numberOfPayments))/
+                (Math.pow((1+ monthlyInterest), numberOfPayments) - 1));
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 }
